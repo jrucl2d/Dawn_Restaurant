6d9f13aaaedcdd53a8854f8cee3dcea465563e2a
jrucl2d <ysk789465@gmail.com>
2020-11-11 22:00:51 +0900

    매장 삭제 구현(경고창 추가해야 함)

--- front/src/component/Store/StoreComponent.jsx
+++ front/src/component/Store/StoreComponent.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
-function StoreComponent({ store }) {
+function StoreComponent({
+  storeInfo,
+  deleteMode,
+  deleteSelected,
+  setDeleteSelected,
+}) {
+  const selectedRef = useRef(null);
+  const onClickDeleteSelect = (e) => {
+    e.preventDefault();
+    // 이미 있었으면 삭제 리스트에서 빼기
+    if (deleteSelected.findIndex((v) => v === e.target.name) !== -1) {
+      setDeleteSelected(deleteSelected.filter((v) => v !== e.target.name));
+      selectedRef.current.className = "card-body";
+    } else {
+      setDeleteSelected([...deleteSelected, e.target.name]);
+      selectedRef.current.className = "card-body deleteSelected";
+    }
+  };
+
   return (
-    <Link className="card-body">
-      <span>{store.storeName}</span>
-    </Link>
+    <>
+      {deleteMode ? (
+        <button
+          onClick={onClickDeleteSelect}
+          className="card-body"
+          ref={selectedRef}
+          name={storeInfo.storeId}
+        >
+          <span>{storeInfo.storeName}</span>
+        </button>
+      ) : (
+        <Link to="#" className="card-body">
+          <span>{storeInfo.storeName}</span>
+        </Link>
+      )}
+    </>
   );
 }
 
--- front/src/component/Store/StoreListComponent.jsx
+++ front/src/component/Store/StoreListComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import StoreRankGraphComponent from "./StoreRankGraphComponent";
 import StoreComponent from "./StoreComponent";
 import "./StoreStyle.css";
@@ -15,6 +15,30 @@ function StoreListComponent() {
       storeId: "weoigjweogjwdljfw",
     },
   ]);
+  const [deleteMode, setDeleteMode] = useState(false);
+  const [deleteSelected, setDeleteSelected] = useState([]);
+
+  const deleteBtnRef = useRef(null);
+
+  const onClickDelete = (e) => {
+    e.preventDefault();
+    if (!deleteMode) {
+      deleteBtnRef.current.className = "btn btn-danger deleting";
+    } else {
+      alert("zzz");
+      setStores(
+        stores.filter((store) => {
+          if (deleteSelected.findIndex((v) => v === store.storeId) === -1) {
+            return store;
+          }
+        })
+      );
+      deleteBtnRef.current.className = "btn btn-warning notDeleting";
+    }
+    setDeleteMode(!deleteMode);
+    setDeleteSelected([]);
+  };
+
   return (
     <div>
       <header>
@@ -24,19 +48,31 @@ function StoreListComponent() {
         <StoreRankGraphComponent />
         <div id="storeList">
           <div className="container">
-            <h2>보유 점포</h2>
-            {stores.map((store) => (
-              <div className="card">
-                <StoreComponent key={store.storeId} store={store} />
+            <h2>
+              {deleteMode ? "삭제할 점포를 모두 선택하세요." : "보유 점포"}
+            </h2>
+            {stores.map((storeInfo) => (
+              <div className="card" key={storeInfo.storeId}>
+                <StoreComponent
+                  storeInfo={storeInfo}
+                  deleteMode={deleteMode ? true : false}
+                  deleteSelected={deleteSelected}
+                  setDeleteSelected={setDeleteSelected}
+                />
               </div>
             ))}
           </div>
           <div className="buttons">
-            <button type="button" class="btn btn-primary">
+            <button type="button" className="btn btn-primary">
               점포 추가
             </button>
-            <button type="button" class="btn btn-danger">
-              점포 삭제
+            <button
+              type="button"
+              className="btn btn-warning notDeleting"
+              ref={deleteBtnRef}
+              onClick={onClickDelete}
+            >
+              {deleteMode ? "삭제하기" : "점포 삭제"}
             </button>
           </div>
         </div>
