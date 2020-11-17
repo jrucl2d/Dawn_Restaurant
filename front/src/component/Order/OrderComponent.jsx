e522c1a0aacc36e6da717d59c52914ee50fba1cf
jrucl2d <ysk789465@gmail.com>
2020-11-17 23:48:29 +0900

    주문 현황판 스타일 적용

--- front/src/component/Order/OrderComponent.jsx
+++ front/src/component/Order/OrderComponent.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 function OrderComponent() {
+  const [foods, setFoods] = useState([
+    {
+      foodId: "!235132525325",
+      foodName: "음식1",
+      foodNum: 4,
+    },
+    {
+      foodId: "2351524twe",
+      foodName: "음식3",
+      foodNum: 2,
+    },
+  ]);
   return (
-    <div>
-      <h4>orders</h4>
+    <div id="orderCard">
+      <div id="foodInfo">
+        {foods.map((food) => (
+          <div key={food.foodId}>
+            {food.foodName} ({food.foodNum}개)
+          </div>
+        ))}
+      </div>
+      <DropdownButton
+        id="dropdown-basic-button"
+        className="my-auto mx-auto"
+        title="새주문"
+      >
+        <Dropdown.Item as="button">조리중</Dropdown.Item>
+        <Dropdown.Item as="button">조리 완료</Dropdown.Item>
+        <Dropdown.Item as="button">음식 수령</Dropdown.Item>
+      </DropdownButton>
     </div>
   );
 }
