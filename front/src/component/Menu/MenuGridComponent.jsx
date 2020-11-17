db7b7890ae8aac2fef4371d441319f2415a2026a
jrucl2d <ysk789465@gmail.com>
2020-11-17 23:53:27 +0900

    주문 현황 서버와의 통신 로직 필요

--- front/src/component/Menu/MenuGridComponent.jsx
+++ front/src/component/Menu/MenuGridComponent.jsx
@@ -13,7 +13,7 @@ function MenuGridComponent() {
         <MenuComponent />
         <MenuComponent />
         <MenuComponent />
-        <MenuComponent /> <MenuComponent />
+        <MenuComponent />
         <MenuComponent />
         <MenuComponent />
         <MenuComponent />
--- front/src/component/Order/OrderComponent.jsx
+++ front/src/component/Order/OrderComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { DropdownButton, Dropdown } from "react-bootstrap";
 
 function OrderComponent() {
